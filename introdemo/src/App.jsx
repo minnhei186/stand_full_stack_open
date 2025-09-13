5c281fc904275e39ca10be239fda4967527178d9
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-09-13 12:30:28 +0900

    コンポーネント　header

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,3 +1,5 @@
+import Header from './Header.jsx'
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 ='Fundamentals of React'
@@ -9,7 +11,7 @@ const App = () => {
 
 return (
   <div>
-    <h1>{course}</h1>
+    <Header text={course}/>
     <p>
       {part1} {exercises1}
     </p>
