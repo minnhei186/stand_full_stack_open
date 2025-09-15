c3e4823f6e6ed6720e2349babd99a7ddbe7627f3
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-09-16 08:30:27 +0900

    refresh and component object {}

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -2,17 +2,10 @@ import Header from './Header.jsx'
 import Content from './Content.jsx'
 import Total from './Total.jsx'
 
-const App = () => {
-   const course = 'Half Stack application development'
-   const part1 = { text: 'Fundamentals of React', exercise: 10}
-   const part2 = { text: 'Using props to pass data' , exercise: 7}
-   const part3 = { text: 'State of a component', exercise: 14}
-return (
-  <div>
-    <Header text={course} />
-    <Content part1={part1} part2={part2} part3={part3}/>
-    <Total x={part1.exercise} y={part2.exercise} z={part3.exercise} />
-  </div>
+const App = (props) => {
+  const {counter} = props
+   return (
+  <div>{counter}</div>
 )
 }
 
--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -1,4 +1,18 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 0
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const refresh = () => {
+    root.render(
+        <App counter={counter} />
+    )
+}
+
+setInterval(()=> {
+    refresh()
+    counter += 1
+},1000)
+
+setInterval()
