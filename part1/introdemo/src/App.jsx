8bd1c954e32f3063cd295b17ec3020bbce535352
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-09-19 08:11:03 +0900

    reset button

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
 
-    console.log('rendering...', counter)
-
+  //const handleClick = () => {
+  //  console.log('clicked')
+  //}
   return (
+    <>
     <div>{counter}</div>
+    <button onClick={() => setCounter(counter+1)}>plus</button>
+    <button onClick={() => setCounter(0)}>reset</button>
+    </>
   )
 }
 
