4e2be0ab21b352a60193144f2cd672f939deb6f9
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-09-22 07:41:28 +0900

    複雑な状態について

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -5,32 +5,20 @@ const Display = ({counter}) => <div>{counter}</div>
 const Button = ({onClick,text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
- console.log('rendering with counter value', counter)
-  //const handleClick = () => {
-  //  console.log('clicked')
-  //}
-  const increaseByone = () =>{
-     console.log('increasing, value before', counter)
-     setCounter(counter + 1)
-  }
-  const decreseByone = () =>{
-    console.log('increasing, value before', counter)
-    setCounter(counter-1);
-}
-
-  const setToZero = () => {
-     console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  const [left,setLeft] = useState(0)
+  const [right,setRight] = useState(0)
 
   return (
-    <>
-    <Display counter={counter}/>
-    <Button onClick={increaseByone} text='plus' />
-    <Button onClick={setToZero} text='reset' />
-    <Button onClick={decreseByone} text='minu' />
-    </>
+    <div>
+      {left}
+      <button onClick={() => setLeft(left+1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right+1)}>
+        right
+      </button>
+      {right}
+    </div>
   )
 }
 
