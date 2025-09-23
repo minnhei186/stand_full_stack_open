d9b0ea0d6b6200d648531e5ef2eb5296ae0674ae
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-09-24 07:47:48 +0900

    note

--- part2/introdemo/src/App.jsx
+++ part2/introdemo/src/App.jsx
@@ -1,36 +1,15 @@
-import { useState } from 'react'
 
-const History = (props) => {
-  if(props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history : {props.allClicks.join()}
-    </div>
-  )
-}
-
-const Button = (props) => (
-  <button onClick={props.onClick}>{props.text}</button>
-)
-
-const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
+const App = (props) => {
+  const { notes } = props
 
   return (
     <div>
-      {value}
-      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
     </div>
   )
 }
