b67e4725f25d0070dd4a788c080648e0d3ed45c0
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-09-26 10:09:25 +0900

    依存配列とバッチとeffectと実行と再レンダリング

--- part2/introdemo/src/App.jsx
+++ part2/introdemo/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from "./components/Note"
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fullfilled')
+        setNotes(response.data)
+      })
+    }, [])
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
@@ -21,13 +32,12 @@ const App = (props) => {
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
+
   const changeShow = () => {
     setShowAll(!showAll)
   }
 
   const notesToshow = showAll ? notes : notes.filter(note => note.important === true)
-
-
 
   return (
     <div>
