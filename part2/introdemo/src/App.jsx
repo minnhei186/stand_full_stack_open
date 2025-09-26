eee5b1a4d309524295f5e40e14dc66d8fbc385c5
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-09-26 16:14:12 +0900

    識別　データ　更新 同期

--- part2/introdemo/src/App.jsx
+++ part2/introdemo/src/App.jsx
@@ -41,6 +41,13 @@ const App = () => {
   }
 
   const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
     console.log(`importance of ${id} needs to be toggled`)
   }
 
