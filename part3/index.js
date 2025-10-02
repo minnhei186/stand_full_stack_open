fa94d88b9634f30cb1ede6bd2a5ec04be56734a5
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-10-03 07:33:21 +0900

    post new note

--- part3/index.js
+++ part3/index.js
@@ -29,9 +29,29 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
 app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
+    const body = request.body
+
+    if(!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
     response.json(note)
 })
 
