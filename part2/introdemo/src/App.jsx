ce5ef05591a0600fbcf0a75207dd54fa748b1683
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-10-06 07:50:33 +0900

    use services

--- part2/introdemo/src/App.jsx
+++ part2/introdemo/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from "./components/Note"
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -9,10 +10,9 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
+    noteService
+      .getAll()
       .then(response => {
-        console.log('promise fullfilled')
         setNotes(response.data)
       })
     }, [])
@@ -28,8 +28,8 @@ const App = () => {
       content: newNote,
       important: Math.random() <0.5,
     }
-    axios
-     .post('http://localhost:3001/notes', noteObject)
+    noteService
+     .create(noteObject)
      .then(response => {
       setNotes(notes.concat(response.data))
       setNewNote('')
@@ -45,7 +45,9 @@ const App = () => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important}
 
-    axios.put(url, changedNote).then(response => {
+    noteService
+    .update(id, changedNote)
+    .then(response => {
       setNotes(notes.map(note => note.id === id ? response.data : note))
     })
     console.log(`importance of ${id} needs to be toggled`)
