8dce59c7cb8b81e8e230f581185e8f21f6c77982
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-10-07 08:31:58 +0900

    フロント、バック接続　cors

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
-//appオブジェクトのuseメソッドでパースのためのjsonパースを用意する
+app.use(cors())
 app.use(express.json())
 
 let notes = [
